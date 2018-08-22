f542d7d8dc16fe2d71e5c6aa875bcbfe72cca99e
rhymedys <rhymedys@gmail.com>
2018-08-22 17:54:32 +0800

    增加检测性能

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@
  * @Author: Rhymedys/Rhymedys@gmail.com
  * @Date: 2018-08-15 16:23:33
  * @Last Modified by: Rhymedys
- * @Last Modified time: 2018-08-21 16:34:29
+ * @Last Modified time: 2018-08-22 17:50:16
  */
 let errorList = []
 let addData = {}
+let appId = null
 let opt = {
   // 上报地址
   reportApi: 'http://localhost/api',
@@ -68,6 +69,64 @@ let callBack = null
 
 function checkIsFunction (obj) {
   return Object.prototype.toString.call(obj) === '[object Function]'
+}
+
+// 统计页面性能
+function performancePage () {
+  if (!window.performance) return
+  let timing = window.performance.timing
+  conf.performance = {
+    // DNS解析时间
+    dnst: timing.domainLookupEnd - timing.domainLookupStart || 0,
+    // TCP建立时间
+    tcpt: timing.connectEnd - timing.connectStart || 0,
+    // 白屏时间
+    wit: timing.responseStart - timing.navigationStart || 0,
+    // dom渲染完成时间
+    domt: timing.domContentLoadedEventEnd - timing.navigationStart || 0,
+    // 页面onload时间
+    lodt: timing.loadEventEnd - timing.navigationStart || 0,
+    // 页面准备时间
+    radt: timing.fetchStart - timing.navigationStart || 0,
+    // 页面重定向时间
+    rdit: timing.redirectEnd - timing.redirectStart || 0,
+    // unload时间
+    uodt: timing.unloadEventEnd - timing.unloadEventStart || 0,
+    // request请求耗时
+    reqt: timing.responseEnd - timing.requestStart || 0,
+    // 页面解析dom耗时
+    andt: timing.domComplete - timing.domInteractive || 0
+  }
+}
+
+// 统计页面资源性能
+function performanceResource () {
+  if (!window.performance && !window.performance.getEntries) return false
+  let resource = window.performance.getEntriesByType('resource')
+
+  let resourceList = []
+  if (!resource && !resource.length) return resourceList
+
+  resource.forEach((item) => {
+    let json = {
+      name: item.name,
+      method: 'GET',
+      type: item.initiatorType,
+      duration: item.duration.toFixed(2) || 0,
+      decodedBodySize: item.decodedBodySize || 0,
+      nextHopProtocol: item.nextHopProtocol
+    }
+    if (conf.ajaxMsg && conf.ajaxMsg.length) {
+      for (let i = 0, len = conf.ajaxMsg.length; i < len; i++) {
+        if (conf.ajaxMsg[i].url === item.name) {
+          json.method = conf.ajaxMsg[i].method || 'GET'
+          json.type = conf.ajaxMsg[i].type || json.type
+        }
+      }
+    }
+    resourceList.push(json)
+  })
+  conf.resourceList = resourceList
 }
 
 // 比较onload与ajax时间长度
@@ -143,8 +202,8 @@ function injectListener2Ajax (funs) {
 
 function reportData () {
   setTimeout(() => {
-    // if (opt.isPage) perforPage()
-    // if (opt.isResource) perforResource()
+    if (opt.isPage) performancePage()
+    if (opt.isResource) performanceResource()
     if (errorList && errorList.length) conf.errorList = conf.errorList.concat(errorList)
     let w = document.documentElement.clientWidth || document.body.clientWidth
     let h = document.documentElement.clientHeight || document.body.clientHeight
@@ -157,6 +216,7 @@ function reportData () {
       performance: conf.performance,
       resourceList: conf.resourceList,
       addData,
+      appId,
       screenwidth: w,
       screenheight: h
     }
@@ -205,6 +265,7 @@ function ajaxResponse (xhr, type) {
     status: xhr.status,
     payload: xhr.xhr.body
   }
+
   conf.errorList.push(defaults)
 }
 
@@ -343,8 +404,43 @@ function attachLoadEventListener () {
   }, false)
 }
 
+/**
+ * 从脚本连接中获取appId
+ *
+ */
+function initAppId (options) {
+  if (options && options.appId) {
+    appId = options.appId
+    delete options.appId
+  } else {
+    const scriptList = Array.from(document.getElementsByTagName('script'))
+    const reg = new RegExp(/loshi-report.*?js/)
+    const res = scriptList.find(val => reg.test(val.getAttribute('src')))
+    res && (appId = res.getAttribute('src').split('?')[1])
+  }
+}
+
 class Report {
+  /**
+   * 初始化
+   *
+   * @param {*} options
+   * @param {*} cb
+   * @returns
+   * @memberof Report
+   */
   init (options, cb) {
+    Object.defineProperty(window, 'LoshiReport', {
+      value: this,
+      configurable: false,
+      writable: false
+    })
+    Object.freeze(window.LoshiReport)
+    /* eslint no-proto: "error" */
+    Object.freeze(window.LoshiReport.__proto__)
+
+    initAppId(options)
+
     opt = Object.assign(opt, options)
     callBack = cb
 
@@ -376,9 +472,9 @@ class Report {
   }
 
   addData (cb) {
-    if (checkIsFunction(cb) && cb) {
-      return cb(addData)
-    }
+    checkIsFunction(cb) && cb(addData)
+
+    return this
   }
 }
 
